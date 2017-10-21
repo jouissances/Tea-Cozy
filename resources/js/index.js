a27bc235aa8aa84679b492aff721dbeccf766469
Janice Darikho <janicedarikho@gmail.com>
2017-10-21 11:47:34 +0800

    added golden touches

---
+++ resources/js/index.js
@@ -0,0 +1,9 @@
+
+$(document).alton({
+    firstClass: 'background',
+    singleSlideClass: 'slide', // Tell Alton the full height slide class
+    useSlideNumbers: true, // Set to false if you don't want to use pagination
+    slideNumbersBorderColor: '#fff', // Set the outside color of the pagination items
+    slideNumbersColor: 'transparent', // Set the inner color of the pagination items
+    bodyContainer: 'pageWrapper', // Tell Alton the body class
+});
